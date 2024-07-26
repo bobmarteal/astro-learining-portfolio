ece241428661fb45f424a4cd4345f86bd82774b8
bob <bobmarteal@gmail.com>
2024-07-26 15:52:08 -0400

    first build of lesson portfolio

---
+++ src/data/siteData.ts
@@ -0,0 +1,32 @@
+export const baseData = {
+  title: "Katie Haus Portfolio",
+  description: "This is a site with all of my stuff",
+};
+
+export const socialData = [
+  {
+    title: "dribbble",
+    href: "https://dribbble.com/",
+    icon: "icon--dribbble",
+  },
+  {
+    title: "instagram",
+    href: "https://instagram.com/",
+    icon: "icon--instagram",
+  },
+  {
+    title: "twitter",
+    href: "https://twitter.com/",
+    icon: "icon--twitter",
+  },
+  {
+    title: "youtube",
+    href: "https://youtube.com/",
+    icon: "icon--youtube",
+  },
+  {
+    title: "github",
+    href: "https://github.com/",
+    icon: "icon--github",
+  },
+];
